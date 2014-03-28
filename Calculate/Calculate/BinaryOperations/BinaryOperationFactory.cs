22ccb0873999838853a9a8eb5171bc8eb9b01970
craftone165 <craftone165@gmai.com>
2014-03-28 10:31:03 +0700

    Created factory and moved basic operations classes

---
+++ Calculate/Calculate/BinaryOperations/BinaryOperationFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculate.BinaryOperations
+{
+    public static class BinaryOperationFactory
+    {
+        public static IBinaryOperation CreateOperation(string name)
+        {
+            switch (name)
+            {
+                case "+":
+                    return new Adder();
+                case "-":
+                    return new Substracter();
+                case "/":
+                    return new Deducater();
+                case "*":
+                    return new Multiplicater();
+                default:
+                    throw new ArgumentException("Неизвестный калькулятор","name");
+            }
+        }
+    }
+}
--- Calculate/Calculate/BinaryOperations/Substracter.cs
+++ Calculate/Calculate/BinaryOperations/Substracter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Calculate
+﻿namespace Calculate.BinaryOperations
 {
     class Substracter : IBinaryOperation
     {
