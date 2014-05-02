3e3824578e25a919c5a77cb27d87e8dca1613e9f
craftone165 <craftone165@gmail.com>
2014-05-02 11:10:19 +0700

    Tests for sorting

---
+++ Calculate/Calculate.Tests/SortingOperations/BogoSortTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculate.SortingOperations;
+using NUnit.Framework;
+
+namespace Calculate.Tests.SortingOperations
+{
+    public class BogoSortTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            ISortingOperation calculator = SortingOperationsFactory.CreateOperation("bogosort");
+            int[] result = calculator.Sort(new []{1,6,3,76});
+            int[] expected = new[] {1, 3, 6, 76};
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
---
+++ Calculate/Calculate.Tests/SortingOperations/QuickSortTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculate.SortingOperations;
+using NUnit.Framework;
+
+namespace Calculate.Tests.SortingOperations
+{
+    public class QuickSortTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            ISortingOperation calculator = SortingOperationsFactory.CreateOperation("quicksort");
+            int[] result = calculator.Sort(new []{1,6,3,76});
+            int[] expected = new[] {1, 3, 6, 76};
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+        [Test]
+        public void SecondSortTest()
+        {
+            ISortingOperation calculator = SortingOperationsFactory.CreateOperation("quicksort");
+            int[] result = calculator.Sort(new[] { 156, 156, 76, 54, -5,-14});
+            int[] expected = new[] {-14, -5, 54, 76, 156, 156};
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
